45e84cdfc802bd88e6e73935ccfb32c983cef204
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-01 14:19:09 +0900

    Feat : Add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/navbar";
+import Home from './components/page/Home';
+import About from './components/page/About';
 import "./App.css";
 
 const App = () => {
-  return <div className="App">My App</div>;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
