327dcffc82f4c4b6a897fcbcc77d6134cc41e533
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-01 11:43:51 +0900

    Feat : add Router, controller for auth user

---
+++ src/controller/auth.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { checkValidation } = require('../middlewares/data-validator');
+
+const User = require('../models/user');
+
+const controller = {
+  login: async (req, res) => {
+    // body data validation check
+    checkValidation(req, res);
+    const { email, password } = req.body;
+
+    try {
+      // check duple user
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: '아이디 또는 비밀번호가 일치하지 않습니다' });
+      }
+
+      // check password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: '아이디 또는 비밀번호가 일치하지 않습니다' });
+      }
+
+      // jwt 토큰 생성 후 response로 반환
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      const token = await jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: 3600
+      });
+
+      res.json({ token });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+};
+
+module.exports = controller;
--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,19 +1,17 @@
 const express = require('express');
+const { validator } = require('../../middlewares/data-validator');
+const authController = require('../../controller/auth');
 
 const router = express.Router();
 
 // @route     GET api/auth
 // @desc      Get logged in user
 // @access    Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
-});
+// router.get('/', validator.auth, authController.login);
 
 // @route     POST api/auth
 // @desc      Auth user & get token
 // @access    Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post('/', validator.auth, authController.login);
 
 module.exports = router;
