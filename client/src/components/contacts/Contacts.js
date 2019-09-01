b910564a3f79acc99b0c30b64764126c40929ae9
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-01 14:54:55 +0900

    Feat : context 선언 및 초기화 작업

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactContext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Contacts
