f68d17a24d0985bb901ee7f7cc6ac937eaefb489
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-02 22:11:14 +0900

    Feat : form clear 기능 구현
    clear 버튼을 누르면 current 초기화, form clear

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -4,7 +4,7 @@ import ContactContext from "../../context/contact/contactContext";
 const ContactForm = () => {
   const contactContext = useContext(ContactContext);
 
-  const { addContact, current } = contactContext;
+  const { addContact, clearCurrent, current } = contactContext;
 
   useEffect(() => {
     if (current !== null) {
@@ -15,9 +15,9 @@ const ContactForm = () => {
         email: "",
         phone: "",
         type: "personal"
-      })
+      });
     }
-  }, [contactContext, current])
+  }, [contactContext, current]);
 
   const [contact, setContact] = useState({
     name: "",
@@ -33,7 +33,7 @@ const ContactForm = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    contactContext.addContact(contact);
+    addContact(contact);
     setContact({
       name: "",
       email: "",
@@ -41,6 +41,10 @@ const ContactForm = () => {
       type: "personal"
     });
   };
+
+  const clearAll = () => {
+    clearCurrent();
+  }
 
   return (
     <form onSubmit={onSubmit}>
@@ -92,6 +96,11 @@ const ContactForm = () => {
           className="btn btn-primary btn-block"
         />
       </div>
+      {current && <div>
+        <button className="btn btn-light btn-block" onClick={clearAll}>
+          Clear
+        </button>
+      </div>}
     </form>
   );
 };
