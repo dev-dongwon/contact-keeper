798bd99368b8d570142cb2bd11e19f2eb45ebb42
dev-dongwon <dev.dongwonkim@gmail.com>
2019-09-02 17:00:51 +0900

    Feat : addContact dispatch 추가

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -42,6 +42,10 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
 
@@ -57,13 +61,14 @@ const ContactState = props => {
 
   return (
     <ContactContext.Provider
-    value={{
-      contacts: state.contacts
-    }}
+      value={{
+        contacts: state.contacts,
+        addContact
+      }}
     >
-      { props.children }
+      {props.children}
     </ContactContext.Provider>
-  )
+  );
 };
 
 export default ContactState;
